9c1106a63062068a129579ed872ecd78888e78a1
SoyKat? <164456237+Soy-Katsito@users.noreply.github.com>
2025-04-02 03:18:10 +0000

    Main Code Upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+#define RESET "\033[0m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define GRAY "\033[90m"
+#define WHITE "\033[97m"
+
+int main() {
+  cout << GREEN; 
+  cout << " ▄▄▄▄ ▓██   ██▓▄▄▄█████▓▓█████   ██████ ▄▄▄█████▓ ▒█████   ██▀███   ███▄ ▄███▓\n";
+  cout << "▓█████▄▒██  ██▒▓  ██▒ ▓▒▓█   ▀ ▒██    ▒ ▓  ██▒ ▓▒▒██▒  ██▒▓██ ▒ ██▒▓██▒▀█▀ ██▒\n";
+  cout << "▒██▒ ▄██▒██ ██░▒ ▓██░ ▒░▒███   ░ ▓██▄   ▒ ▓██░ ▒░▒██░  ██▒▓██ ░▄█ ▒▓██    ▓██░\n";
+  cout << "▒██░█▀  ░ ▐██▓░░ ▓██▓ ░ ▒▓█  ▄   ▒   ██▒░ ▓██▓ ░ ▒██   ██░▒██▀▀█▄  ▒██    ▒██ \n";
+  cout << "░▓█  ▀█▓░ ██▒▓░  ▒██▒ ░ ░▒████▒▒██████▒▒  ▒██▒ ░ ░ ████▓▒░░██▓ ▒██▒▒██▒   ░██▒\n";
+  cout << "░▒▓███▀▒ ██▒▒▒   ▒ ░░   ░░ ▒░ ░▒ ▒▓▒ ▒ ░  ▒ ░░   ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░░ ▒░   ░  ░\n";
+  cout << "▒░▒   ░▓██ ░▒░     ░     ░ ░  ░░ ░▒  ░ ░    ░      ░ ▒ ▒░   ░▒ ░ ▒░░  ░      ░\n";
+  cout << " ░    ░▒ ▒ ░░    ░         ░   ░  ░  ░    ░      ░ ░ ░ ▒    ░░   ░ ░      ░   \n";
+  cout << " ░     ░ ░                 ░  ░      ░               ░ ░     ░            ░   \n";
+  cout << "      ░░ ░                                                                    \n\n";
+
+
+  cout << YELLOW; 
+  cout << "¡Bienvenido a QUIZO!\n\n";
+
+  cout << WHITE; 
+  cout << "Creado originalmente por ByteStorm\n";
+  cout << "Versión optimizada por Katsito y Charmander58\n";
+
+  cout << GRAY; 
+  cout << "\n© 2024-2025 ByteStorm - Todos los derechos reservados\n";
+  cout << "© 2021-2025 Chardev's team - Todos los derechos reservados\n";
+
+  cout << RESET; 
+  return 0;
+}
